4e604012703ee7f6068b6cf94dd6a37873e17ede
Nazim Shala <shalanazim@gmail.com>
2019-07-29 10:36:50 +0200

    Add Spinner, Add animation

--- script/index.js
+++ script/index.js
@@ -1,35 +1,58 @@
 window.addEventListener("DOMContentLoaded", () => {
   const mainContainer = document.querySelector(".main-container");
   const newAdvice = document.querySelector("#new-advice");
-  newAdvice.addEventListener("click", getNewAdvice, false);
+  newAdvice.addEventListener("click", setlogo, false);
+  let title = document.querySelector("#title");
+  title.addEventListener("animationend", getNewAdvice);
+  let spinner = document.querySelector(".spin")
   let adviceSource = document.querySelector("#advice-template").innerHTML;
   let adviceTemplate = Handlebars.compile(adviceSource);
   
-function getNewAdvice(data) {
-  fetch(`https://api.adviceslip.com/advice`)
-.then(function(response) {
-  if (response.ok) {
-    return response.json();
-  }else{
-    throw new Error("got no data");
-  }
-})
-.then(function(data){
-  if (data) {
-  let advice = adviceTemplate({advice: data.slip.advice});
-  mainContainer.innerHTML = advice;
-  }else{
-    let noData = adviceTemplate({advice: "Got no more free Advice"});
-  mainContainer.innerHTML += noData;
-  }
+  function setlogo() {
+    if (spinner.childNodes[1].classList == "") {
+      spinner.childNodes[1].classList.add("spinner");
+      spinner.childNodes[3].innerHTML ="...loading..."
+      mainContainer.innerHTML = "";
+      mainContainer.appendChild(spinner);
+    }else{
+      mainContainer.innerHTML = "";
+      mainContainer.appendChild(spinner);
+    }
+    if (title.classList == "title-start" ) {
+      title.classList.remove("title-start");
+      title.classList.add("title-logo");
+    }
+    if (newAdvice.innerHTML == "Next Advice") {
+      setTimeout(() => { 
+        getNewAdvice();
+      },1000)
+    }
+  };
   
-
-})
-.catch(function(err){
-  let noData = adviceTemplate({advice: err});
-  mainContainer.innerHTML += noData;
-})
-}
-
-
+  function getNewAdvice(data) {
+    fetch(`https://api.adviceslip.com/advice`)
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw new Error("got no data");
+      }
+    })
+    .then(function(data){
+      if (data) {
+        let advice = adviceTemplate({advice: data.slip.advice});
+        mainContainer.innerHTML = advice;
+      }else{
+        let noData = adviceTemplate({advice: "Got no more free Advice"});
+        mainContainer.innerHTML += noData;
+      }
+    })
+    .catch(function(err){
+      let noData = adviceTemplate({advice: err});
+      mainContainer.innerHTML += noData;
+    })
+    newAdvice.innerHTML = "Next Advice";
+    mainContainer.removeChild(spinner);
+  };
+  
 });
